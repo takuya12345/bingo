4c90f7c814cd4b68d78d5cc1a223d925dcd0b601
takuya12345 <10l.mah.s11@gmail.com>
2022-10-17 06:56:27 +0900

    feat: ビンゴの数字を並べました

--- index.js
+++ index.js
@@ -4,14 +4,14 @@
     function createColumn(col) {
         const source = [];
         for (let i = 0; i < 15; i++) {
-            source[i] = i + 1 * col;
+            source[i] = i + 1 + 15 * col;
         }
 
-        const columns = [];
+        const column = [];
         for (let i = 0; i < 5; i++) {
-            columns[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+            column[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
         }
-        return columns;
+        return column;
     }
 
     const columns = [];
@@ -20,5 +20,25 @@
     columns[2] = createColumn(2);
     columns[3] = createColumn(3);
     columns[4] = createColumn(4);
-    console.table(columns);
+    columns[2][2] = 'FREE';
+
+    const bingo = [];
+    for (let row = 0; row < 5; row++) {
+        bingo[row] = [];
+
+        for (let col = 0; col < 5; col++) {
+            bingo[row][col] = columns[col][row];
+        }
+    }
+    console.table(bingo);
+
+    for (let row = 0; row < 5; row++) {
+        const tr = document.createElement('tr');
+        for (let col = 0; col < 5; col++) {
+            const td = document.createElement('td');
+            td.textContent = bingo[row][col];
+            tr.appendChild(td);
+        }
+    document.querySelector('tbody').appendChild(tr);
+    }
 }
